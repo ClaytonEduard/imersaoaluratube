780ce63860a70f8f5bf7a2dd1d769c37cd42e0d6
ClaytonEduard <clayton_eduard@hotmail.com>
2022-11-10 23:54:33 -0300

    Primeiro Commit

---
+++ dist/next.config.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/** @type {import('next').NextConfig} */
+var nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  }
+};
+module.exports = nextConfig;
